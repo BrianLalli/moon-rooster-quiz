49adf929e81e631d2806bde8ad743911d475d335
Brian Lalli <106120015+BrianLalli@users.noreply.github.com>
2024-01-17 17:34:57 -0500

    added new quiz topics and updated svg icons

--- src/data/quizTopics.tsx
+++ src/data/quizTopics.tsx
@@ -1,14 +1,19 @@
 import { ReactNode } from 'react'
-import { ReactComponent as Angular } from '../assets/icons/angular.svg'
+// import { ReactComponent as Angular } from '../assets/icons/angular.svg'
 import { ReactComponent as CSS } from '../assets/icons/css-3.svg'
-import { ReactComponent as Django } from '../assets/icons/dj.svg'
-import { ReactComponent as Gatsby } from '../assets/icons/gatsby.svg'
+// import { ReactComponent as Django } from '../assets/icons/dj.svg'
+// import { ReactComponent as Gatsby } from '../assets/icons/gatsby.svg'
 import { ReactComponent as JavaScript } from '../assets/icons/javascript.svg'
-import { ReactComponent as Kotlin } from '../assets/icons/kotlin.svg'
-import { ReactComponent as Laravel } from '../assets/icons/laravel.svg'
+// import { ReactComponent as Kotlin } from '../assets/icons/kotlin.svg'
+// import { ReactComponent as Laravel } from '../assets/icons/laravel.svg'
 import { ReactComponent as Python } from '../assets/icons/python.svg'
 import { ReactComponent as ReactIcon } from '../assets/icons/react.svg'
 import { ReactComponent as BulbIcon } from '../assets/icons/bulb.svg'
+import {ReactComponent as SQL } from '../assets/icons/sql-database-sql-azure.svg'
+import {ReactComponent as TheOffice } from '../assets/icons/OFFICE-LOGO.svg'
+import {ReactComponent as Referee } from '../assets/icons/referee.svg'
+import {ReactComponent as Mixology } from '../assets/icons/cocktail-glass.svg'
+import {ReactComponent as Steelers } from '../assets/icons/steelers.svg'
 
 type QuizTopic = {
   title: string
@@ -36,7 +41,7 @@ export const quizTopics: QuizTopic[] = [
   },
   {
     title: 'SQL',
-    icon: <Laravel />,
+    icon: <SQL />,
   },
   {
     title: 'General Knowledge',
@@ -44,19 +49,18 @@ export const quizTopics: QuizTopic[] = [
   },
   {
     title: 'The Office',
-    icon: <Gatsby />,
-    disabled: false,
+    icon: <TheOffice />,
   },
   {
     title: 'Pittsburgh Steelers',
-    icon: <Angular />,
+    icon: <Steelers />,
   },
   {
     title: 'Football Officiating',
-    icon: <Django />
+    icon: <Referee />
   },
   {
     title: 'Mixology',
-    icon: <Kotlin />,
+    icon: <Mixology />,
   },
 ]
